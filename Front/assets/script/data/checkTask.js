fe6511bf8118dfb727227df444ccf5342501e12d
lokywolf2295 <matiasesek1995@gmail.com>
2023-09-25 22:37:25 -0300

    Update(Front): complete checkTask method

---
+++ Front/assets/script/data/checkTask.js
@@ -0,0 +1,31 @@
+import { path } from "../data/path.js";
+
+export const checkTask = async (id,finished) => {
+  const url = path + "mark_as_finished/" + id+"/"+finished;
+  console.log(url);
+  fetch(url, {
+    method: "PATCH",
+  })
+    .then((res) => {
+      if (res.ok) {
+        window.location.href = "index.html";
+      } else {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "No se actualizó correctamente",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    })
+    .catch((error) => {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Error en la solicitud:" + error, // Maneja el error de acuerdo a tus necesidades
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
+};
