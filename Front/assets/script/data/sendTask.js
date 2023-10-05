ea68cd8df9bb87135947999e24476f50034fc5d3
lokywolf2295 <matiasesek1995@gmail.com>
2023-10-04 21:47:50 -0300

    Update(Front): modify taskObject and delete script for localstorage in sendTask.js.

--- Front/assets/script/data/sendTask.js
+++ Front/assets/script/data/sendTask.js
@@ -1,12 +1,16 @@
 import { path } from "./path.js";
 
 export const sendTask = (taskObj) => {
-  console.log(taskObj)  
-  const url = `${path}create/title=${taskObj.title}&dateFormat=${taskObj.dateFormat}&time=${taskObj.time}`;
+  console.log(taskObj) 
+  const url = `${path}create`;
   //const url = `${path}create/{"title":"${taskObj.title}","date":"${taskObj.dateFormat}","time":"${taskObj.time}"}`;
   console.log(url);
   fetch(url, {
     method: "POST",
+    body: JSON.stringify(taskObj).toString(), // data can be `string` or {object}!
+    headers: {
+      "Content-Type": "application/json",
+    },
   }).then((res) => {
     if (res.ok) {
       window.location.href = "index.html";
