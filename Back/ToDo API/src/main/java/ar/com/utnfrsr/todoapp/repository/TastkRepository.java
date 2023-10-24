de2d4f8c561c5438bcb7f373638c4d3e388fd7aa
lokywolf2295 <matiasesek1995@gmail.com>
2023-10-24 00:14:11 -0300

    Update(Back): remove unused import in TaskRepository class

--- Back/ToDo API/src/main/java/ar/com/utnfrsr/todoapp/repository/TastkRepository.java
+++ Back/ToDo API/src/main/java/ar/com/utnfrsr/todoapp/repository/TastkRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface TastkRepository extends JpaRepository<Task, Long> { //<nombre de la entidad, tipo del identificador que marcamos con @id>
 
      /**
