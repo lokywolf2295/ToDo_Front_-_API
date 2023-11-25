e333d8a5cc64492e741821ad708f5f15e54b8b2e
lokywolf2295 <matiasesek1995@gmail.com>
2023-11-25 07:58:46 -0300

    Update(Back): reformat code to ITaskService interface

--- Back/ToDo API/src/main/java/ar/com/utnfrsr/todoapp/service/ITaskService.java
+++ Back/ToDo API/src/main/java/ar/com/utnfrsr/todoapp/service/ITaskService.java
@@ -8,7 +8,10 @@ import java.util.List;
 public interface ITaskService {
 
     TaskResponseDTO createTask(TaskRequestDTO taskRequestDTO);
+
     List<TaskResponseDTO> findAll();
+
     void updateTaskAsFinished(Long id, boolean finished);
+
     void deleteById(Long id);
 }
